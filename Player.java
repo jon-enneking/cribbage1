a8962798c11718bf56660fa2db84101c51915c8f
jon-enneking <87773019+jon-enneking@users.noreply.github.com>
2021-08-12 09:22:04 -0700

    Added lengthOfRun() and cardsToRanks()
    May not end up needing the cardsToRanks function. Need to add functionality to lengthOfRun where we don't have to tell it a direction to look so that it is able to give the total size of the run given one card in the run.

--- Player.java
+++ Player.java
@@ -11,6 +11,14 @@ public class Player {
         this.hand = new ArrayList<Card>();
     }
 
+    public ArrayList<Integer> cardsToRanks(ArrayList<Card> cards){
+        ArrayList<Integer> rank_hand = new ArrayList<Integer>(cards.size());
+        for(Card card : cards) {
+            rank_hand.add(card.getRank());
+        }
+        return rank_hand;
+    }
+    
     public void displayHand() {
         System.out.println(name + "\'s hand is:");
         for(Card card : hand) {
@@ -62,5 +70,24 @@ public class Player {
         removeCard(c);
         scanny.close();
         return c;
+    }
+
+    /* Determines the length of a run given the rank of the starting card,
+    the direction, and the ArrayList of ranks */
+    public int lengthOfRun(int start, boolean up, ArrayList<Integer> ranks) {
+        int total_distance = 1;
+        int next;
+        if(up) next = 1;
+        else next = -1;
+
+        int index = ranks.indexOf(start+next);
+        if(index != -1) {
+            total_distance++;
+            int removed = ranks.remove(index);
+            total_distance += lengthOfRun(removed, up, ranks);
+            ranks.add(index, removed);
+        }
+
+        return total_distance;
     }
 }
